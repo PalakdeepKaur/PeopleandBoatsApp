006a5916c146a47469157ef4b32d9676c248a413
Palakdeep kaur <pdkaur987@gmail.com>
2020-12-11 22:51:36 -0800

    initial commit

---
+++ client/src/components/lists/Boats.js
@@ -0,0 +1,4 @@
+import React, { useEffect, useState } from 'react'
+import { useMutation } from '@apollo/client'
+import { Form, Input, Button } from 'antd'
+// import { UPDATE_PERSON } from '../../queries'
